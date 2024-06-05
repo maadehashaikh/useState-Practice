334c313c6ab1d797ae441f85065da3f3cba6a9c4
Maadeha shaikh <maadeham@gmail.com>
2024-06-05 21:02:36 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Greetings from './components/Greetings';
 
 function App() {
+  const [counter,setCount] = useState(false);
+  const [greeting,setGreeting] = useState(false);
+  const counterFunc = () => {
+    setCount(true);
+    setGreeting(false);
+  }
+  const greetingFunc = () => {
+    setGreeting(true);
+    setCount(false);
+  }
   return (
     <div className="App">
-      {/* <button>Counter</button>
-      <button>Greeting</button> */}
-      <Counter/>
-      <Greetings/>
+      <button onClick={counterFunc}>Counter</button>
+      <button onClick={greetingFunc}>Greeting</button>
+       {counter && <Counter/>}
+       {greeting && <Greetings/>}
     </div>
   );
 }
